0013b5175bb49e588355bf2872b8d5bbe11994c6
Leonti Bielski <prishelec@gmail.com>
2017-07-17 08:53:02 +1000

    Saving

--- api/lib/index.js
+++ api/lib/index.js
@@ -50,16 +50,22 @@ const apiMiddleware = [jwtCheck, homeUsersCheck, errorHandler]
 
 // /static /static/css/main.1d4c7900.css
 app.use((req, res, next) => {
-  if (!req.url.match(/\/api\/*/g)) {
+  console.log('not NEXT', req.url)
+  if (req.url == '/') {
+    console.log('sending index')
+    res.sendFile(path.resolve(__dirname, '../../client/build/index.html'))
+  } else if (!req.url.match(/\/api\/*/g)) {
     const filePath = req.url.replace('/', '../../client/build/')
     const file = path.resolve(__dirname, filePath)
 
     fs.access(file, fs.constants.R_OK, err => {
-      err ? res.sendFile(path.resolve(__dirname, '../../client/build/index.html')) 
+      console.log(err)
+      err ? res.sendFile(path.resolve(__dirname, '../../client/build/index.html'))
         : res.sendFile(file);
     })
   } else {
-    next();
+    console.log('NEXT', req.url)
+    next()
   }
 });
 
